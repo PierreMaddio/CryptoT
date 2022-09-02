2f38db45383078da8b90f19d66337417cfcfbf01
PierreMaddio <pmsymf@gmail.com>
2022-09-02 11:19:00 +0200

    first commit

---
+++ CryptoT/CryptoTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTApp.swift
+//  CryptoT
+//
+//  Created by Pierre on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
