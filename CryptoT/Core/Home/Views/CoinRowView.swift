9a424bdb4ab102408a51e6191977d07c9ba86f1e
PierreMaddio <pmsymf@gmail.com>
2022-09-09 17:01:30 +0200

    add Coin Model

---
+++ CryptoT/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,21 @@
+//
+//  CoinRowView.swift
+//  CryptoT
+//
+//  Created by Pierre on 09/09/2022.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView()
+    }
+}
