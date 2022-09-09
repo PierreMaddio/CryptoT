1a617e56bcf7774e69f720638160005b3afdaa60
PierreMaddio <pmsymf@gmail.com>
2022-09-09 18:36:15 +0200

    add a dynamic list to display cryptocurrencies

---
+++ CryptoT/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewModel.swift
+//  CryptoT
+//
+//  Created by Pierre on 09/09/2022.
+//
+
+import Foundation
+
+// ObservableObject: observed it from our view
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [Coin] = []
+    @Published var portfolioCoins: [Coin] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+    
+}
--- CryptoT/CryptoTApp.swift
+++ CryptoT/CryptoTApp.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 @main
 struct CryptoTApp: App {
+    // anything in the HomeView as access to the HomeViewModel
+    @StateObject private var vm = HomeViewModel()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 HomeView()
                     .navigationBarHidden(true)
             }
+            .environmentObject(vm)
         }
     }
 }
