aa88d193fe8fbab1b7e4fd4f3c2c782a358f5e0f
PierreMaddio <pmsymf@gmail.com>
2022-09-11 10:35:44 +0200

    add NetworkingManager(reusable Networking layer)

--- CryptoT/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoT/Core/Home/ViewModels/HomeViewModel.swift
@@ -8,13 +8,16 @@
 import Foundation
 import Combine
 
-// ObservableObject: observed it from our view
-// Our view has a reference to this HomeViewModel, this viewModel has a dataService witch initialise a CoinDataService(init getCoins), this getCoins func go to the url, download the data, check is it is valid data, decode that data and then take that data and append to allCoins array.Because allCoins is @Published, in HomeViewModel(init addSubscribers), func addSubscribers subscribing that $allCoins array(Published variable), then append to our allCoins array of HomeViewModel(same name)
+/*
+ ObservableObject: observed it from our view
+ Our view has a reference to this HomeViewModel, this viewModel has a dataService witch initialise a CoinDataService(init getCoins), this getCoins func go to the url, download the data, check is it is valid data, decode that data and then take that data and append to allCoins array.Because allCoins is @Published, in HomeViewModel(init addSubscribers), func addSubscribers subscribing that $allCoins array(Published variable), then append to our allCoins array of HomeViewModel(same name allCoins)
+ */
 class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [Coin] = []
     @Published var portfolioCoins: [Coin] = []
     
+    // dataService initialize CoinDataService(), with init() get the coin utomatically
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
     
--- CryptoT/Services/CoinDataService.swift
+++ CryptoT/Services/CoinDataService.swift
@@ -21,29 +21,11 @@ class CoinDataService {
         // api coingecko: coins, coins/markets: usd, market_cap_desc, 250 results, 1, sparkline(true), 24h price change)
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-        // background thread
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [Coin].self, decoder: JSONDecoder())
-            .sink { (completion) in
-                switch completion {
-                case.finished:
-                    break
-                case.failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
                 self?.coinSubscription?.cancel()
-            }
-            
+            })
     }
 }
