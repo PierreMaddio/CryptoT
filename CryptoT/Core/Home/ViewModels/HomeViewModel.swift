b3c9f37bb3bf2684be418c1650839e1994cc8a2b
PierreMaddio <pmsymf@gmail.com>
2022-09-13 09:29:47 +0200

    add filter on search bar text using Combine

--- CryptoT/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoT/Core/Home/ViewModels/HomeViewModel.swift
@@ -16,6 +16,8 @@ class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [Coin] = []
     @Published var portfolioCoins: [Coin] = []
+    
+    // anything subscribed to the this publisher will then receive that new value
     @Published var searchText: String = ""
     
     // dataService initialize CoinDataService(), with init() get the coin utomatically
@@ -27,12 +29,28 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        // $allCoins is the publisher from CoinDataService
-        dataService.$allCoins
+        
+        // updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [Coin]) -> [Coin] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) || coin.symbol.lowercased().contains(lowercasedText) || coin.id.lowercased().contains(lowercasedText)
+        }
     }
     
 }
