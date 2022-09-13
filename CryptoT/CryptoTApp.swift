28cf42535cd1252681ff00f2b479b40313136757
PierreMaddio <pmsymf@gmail.com>
2022-09-13 17:13:11 +0200

    add PortfolioView to manage portfolio

--- CryptoT/CryptoTApp.swift
+++ CryptoT/CryptoTApp.swift
@@ -12,6 +12,11 @@ struct CryptoTApp: App {
     // anything in the HomeView as access to the HomeViewModel
     @StateObject private var vm = HomeViewModel()
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
